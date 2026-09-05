61004eb3e44a341877bec34c218fcfa07e4045e2
情绪21℃ <xuexiaokang@users.noreply.github.com>
2026-09-06 01:29:20 +0800

    Add openipc-ap-auto.mk for openipc-ap-auto package

---
+++ general/package/openipc-ap-auto/openipc-ap-auto.mk
@@ -0,0 +1,16 @@
+ ################################################################################
+  #
+  # openipc-ap-auto
+  #
+  ################################################################################
+
+  OPENIPC_AP_AUTO_VERSION = 1.0
+  OPENIPC_AP_AUTO_SITE = $(BR2_EXTERNAL)/package/openipc-ap-auto/files
+  OPENIPC_AP_AUTO_LICENSE = MIT
+
+  define OPENIPC_AP_AUTO_INSTALL_TARGET_CMDS
+      $(INSTALL) -m 0755 -D $(@D)/etc/init.d/ssv615x-ap $(TARGET_DIR)/etc/init.d/ssv615x-ap
+      $(INSTALL) -m 0644 -D $(@D)/etc/hostapd.conf $(TARGET_DIR)/etc/hostapd.conf
+  endef
+
+  $(eval $(generic-package))
