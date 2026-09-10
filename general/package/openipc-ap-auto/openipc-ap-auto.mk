3db836dee064969d302215ed29f8559990ab84da
xuexiaokang <xuexiaokang@example.com>
2026-09-11 02:37:57 +0800

    Fix openipc-ap-auto package: use SITE_METHOD=local (GENERAL_PATH)
    The generic-package macro with SITE pointing at files/ made Buildroot try
    to wget a tarball URL -> 404 -> build failed after 7 retries.
    Match the openipc-nfs-root / openipc-precision-time pattern:
    SITE_METHOD=local, SITE=$(BR2_EXTERNAL_GENERAL_PATH)/package/openipc-ap-auto,
    install from $(@D)/files/... No network fetch at all.

--- general/package/openipc-ap-auto/openipc-ap-auto.mk
+++ general/package/openipc-ap-auto/openipc-ap-auto.mk
@@ -2,15 +2,18 @@
 #
 # openipc-ap-auto
 #
+# Auto-start AP mode (hostapd + dnsmasq) on boot for SSV6155P WiFi modules.
+#
 ################################################################################
 
 OPENIPC_AP_AUTO_VERSION = 1.0
-OPENIPC_AP_AUTO_SITE = $(BR2_EXTERNAL)/package/openipc-ap-auto/files
+OPENIPC_AP_AUTO_SITE_METHOD = local
+OPENIPC_AP_AUTO_SITE = $(BR2_EXTERNAL_GENERAL_PATH)/package/openipc-ap-auto
 OPENIPC_AP_AUTO_LICENSE = MIT
 
 define OPENIPC_AP_AUTO_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/etc/init.d/ssv615x-ap $(TARGET_DIR)/etc/init.d/ssv615x-ap
-	$(INSTALL) -m 0644 -D $(@D)/etc/hostapd.conf $(TARGET_DIR)/etc/hostapd.conf
+	$(INSTALL) -m 0755 -D $(@D)/files/etc/init.d/ssv615x-ap $(TARGET_DIR)/etc/init.d/ssv615x-ap
+	$(INSTALL) -m 0644 -D $(@D)/files/etc/hostapd.conf $(TARGET_DIR)/etc/hostapd.conf
 	echo "/etc/init.d/ssv615x-ap start &" >> $(TARGET_DIR)/etc/rc.local
 endef
 
